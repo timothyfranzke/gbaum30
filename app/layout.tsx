99d5736a7a8b56fcd0acb2dcf8ab0750618cb115
timothyfranzke <timothyfranzke@gmail.com>
2025-06-25 14:23:49 -0500

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,17 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        <meta name="og:title" content="Union 30" />
+        <meta name="og:description" content="Union 30 Goalkeeper Union" />
+        <meta name="og:type" content="website" />
+        <meta name="og:url" content="https://union30.com" />
+        <meta name="og:image" content="https://union30.com/og_logo.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Union 30" />
+        <meta name="twitter:description" content="Union 30 Goalkeeper Union" />
+        <meta name="twitter:image" content="https://union30.com/og_logo.png" />
+        
+        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&family=Style+Script&display=swap" rel="stylesheet" />
