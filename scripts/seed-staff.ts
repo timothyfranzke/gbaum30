c17ef95c08dc86e01d616682b4d6abb8f3601784
timothyfranzke <timothyfranzke@gmail.com>
2026-04-21 20:34:03 -0500

    updated with env vars

--- scripts/seed-staff.ts
+++ scripts/seed-staff.ts
@@ -3,7 +3,7 @@ config({ path: '.env.local' });
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON || '{}');
+const serviceAccount = JSON.parse(process.env.NEXT_FIREBASE_SERVICE_ACCOUNT_JSON || '{}');
 
 initializeApp({ credential: cert(serviceAccount) });
 const db = getFirestore();
